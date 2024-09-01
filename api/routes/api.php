7c85985d62265a2d92ba1565d01ad758b83089f4
Gurwinder <47gurvinder@gmail.com>
2024-09-01 22:20:58 +0530

    test route

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,7 @@
 use App\Http\Controllers\ContactFormController;
 
 Route::post('/contact', [ContactFormController::class, 'store']);
-Route::get('/apitest', function (Request $request) {
+Route::get('/', function (Request $request) {
     return response()->json([
         'message' => 'API is working!',
         'status' => 'success'
