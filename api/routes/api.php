5fbdd59270d40c97cb7e5995e5016fe1863956ac
Gurwinder <47gurvinder@gmail.com>
2024-09-01 22:20:17 +0530

    test route

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,3 +2,9 @@
 use App\Http\Controllers\ContactFormController;
 
 Route::post('/contact', [ContactFormController::class, 'store']);
+Route::get('/apitest', function (Request $request) {
+    return response()->json([
+        'message' => 'API is working!',
+        'status' => 'success'
+    ], 200);
+});
--- api/routes/web.php
+++ api/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test2', function (Request $request) {
+    return response()->json([
+        'message' => 'API is working!',
+        'status' => 'success'
+    ], 200);
+});
